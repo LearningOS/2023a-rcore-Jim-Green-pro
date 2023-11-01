d89d00614d2c754023ab8af9d5641e4fd0c888f6
Jim-Green <gy99887766@88.com>
2023-11-01 19:02:15 +0800

    Resolved merge conflicts

--- os/src/sync/up.rs
+++ os/src/sync/up.rs
@@ -1,6 +1,6 @@
 //! Uniprocessor interior mutability primitives
 use core::cell::{RefCell, RefMut};
-use core::sync::atomic::{AtomicBool, Ordering};
+
 /// Wrap a static data structure inside it so that we are
 /// able to access it without any `unsafe`.
 ///
@@ -8,35 +8,9 @@ use core::sync::atomic::{AtomicBool, Ordering};
 ///
 /// In order to get mutable reference of inner data, call
 /// `exclusive_access`.
-
-/// 一个简单的自旋锁实现。
-struct Spinlock {
-    lock: AtomicBool,
-}
-
-impl Spinlock {
-    const fn new() -> Self {
-        Spinlock {
-            lock: AtomicBool::new(false),
-        }
-    }
-
-    /// 尝试获取锁，如果锁已被占用则自旋直到获取。
-    fn lock(&self) {
-        while self.lock.compare_exchange(false, true, Ordering::Acquire, Ordering::Relaxed).is_err() {}
-
-    }
-
-    /// 释放锁。
-    fn unlock(&self) {
-        self.lock.store(false, Ordering::Release);
-    }
-}
-/// 使用自旋锁保护的单处理器内部可变性原语。
 pub struct UPSafeCell<T> {
     /// inner data
     inner: RefCell<T>,
-    lock: Spinlock,
 }
 
 unsafe impl<T> Sync for UPSafeCell<T> {}
@@ -47,14 +21,10 @@ impl<T> UPSafeCell<T> {
     pub unsafe fn new(value: T) -> Self {
         Self {
             inner: RefCell::new(value),
-            lock: Spinlock::new(),
         }
     }
     /// Panic if the data has been borrowed.
     pub fn exclusive_access(&self) -> RefMut<'_, T> {
-        self.lock.lock();
-        let result = self.inner.borrow_mut();
-        self.lock.unlock();
-        result
+        self.inner.borrow_mut()
     }
 }
